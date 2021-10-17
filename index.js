ae408276551bce13d0a9a9c944ecde77b2e8c2a8
Damjan Piperski <damjan.piperski@factoryww.com>
2021-10-17 21:27:44 +0200

    scrape coingecko

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ const cheerio = require('cheerio');
 const app = express();
 
 const articles = [];
+const solana = [];
+const total = [];
 
 app.get('/', (req, res) => {
   res.json('Welcome to my climate change news api');
 });
 
 app.get('/scrape', (req, res) => {
- // axios.get('https://www.theguardian.com/environment/climate-crisis').then((response) => {
-    axios.get('https://www.coingecko.com/en/coins/bitcoin/historical_data/usd#panel').then((response) => {
+  // axios.get('https://www.theguardian.com/environment/climate-crisis').then((response) => {
+  axios.get('https://www.coingecko.com/en/').then((response) => {
     console.log(response.data)
     const html = response.data;
     const $ = cheerio.load(html);
 
-    $('a:contains("climate")', html).each(function() {
+    $('a:contains("climate")', html).each(function () {
       const title = $(this).text();
       const url = $(this).attr('href');
       articles.push({
@@ -27,17 +29,23 @@ app.get('/scrape', (req, res) => {
 
     })
 
-    $('tr', html).each(function() {
-      const currency = $( '.no-wrap').text()
+    $('td:nth-of-type(3) a:first-of-type', 'tr', html).each(function () {
+      const currencyName = $(this).text();
       articles.push({
-        currency
-      })
+        currencyName
+      });
     })
 
+    $('td:nth-of-type(4) span', 'tr', html).each(function (e, i) {
+      const currencyValue = $(this).text();
+      total.push({
+        ...articles[e],
+        currencyValue
+      });
+    })
 
-  res.json(articles)
+    res.json(total)
   }).catch((err) => console.log(err))
-
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
